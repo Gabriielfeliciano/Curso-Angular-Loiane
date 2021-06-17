ccfab3bb91f93b82a705f0b1020e16653d2f53c9
Gabriielfeliciano <gabriel.feliciano123@gmail.com>
2021-06-17 10:02:52 -0300

    Interpolação, Property Binding e Style Binding

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  // title = "Hello Word!"
+  // image = 'https://io2.convertiez.com.br/m/doceefesta/shop/products/images/8652/medium/aranha-halloween-ref-kriat-un_8519.jpg';
+
+  // getValor(){
+  //   return 1 + 1;
+  // }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
