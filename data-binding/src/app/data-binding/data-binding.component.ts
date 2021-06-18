6b5a8b70d66c05aab9b6751e0ac13b76f4bd3f56
Gabriielfeliciano <gabriel.feliciano123@gmail.com>
2021-06-18 11:18:42 -0300

    Event Binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,16 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
-  // title = "Hello Word!"
-  // image = 'https://io2.convertiez.com.br/m/doceefesta/shop/products/images/8652/medium/aranha-halloween-ref-kriat-un_8519.jpg';
+  title = "Hello Word!"
+  image = 'https://io2.convertiez.com.br/m/doceefesta/shop/products/images/8652/medium/aranha-halloween-ref-kriat-un_8519.jpg';
 
-  // getValor(){
-  //   return 1 + 1;
-  // }
+  getValor(){
+    return 1 + 1;
+  }
+
+  valorAtual = "";
+  valorSalvo = "";
+  isMouseOver = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  botaoClicado(){
+    alert('Botão foi clicado!')
+  }
+
+  onKey(evento: KeyboardEvent){
+    this.valorAtual = ((<HTMLInputElement>evento.target).value);
+  }
+
+  salvarValor(valor:string){
+    this.valorSalvo = valor
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver
   }
 
 }
