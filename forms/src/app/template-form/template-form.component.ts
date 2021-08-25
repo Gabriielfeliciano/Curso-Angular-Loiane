d743a1712a6a01c7bacd68fb32c345b78e692984
Gabriielfeliciano <gabriel.feliciano123@gmail.com>
2021-08-25 12:02:34 -0300

    first commit forms

---
+++ forms/src/app/template-form/template-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+  onSubmit(form: any){
+    console.log(form)
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
